1735cbcd993f85b2a5a8ed4da78ff61f819eda72
oletilwe12 <loatemophuting@gmail.com>
2022-09-08 13:01:38 +0200

    final build

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': '8c866e57e8msh2f396bc5fa3ced4p1f2807jsnab7457a3f717',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '8c866e57e8msh2f396bc5fa3ced4p1f2807jsnab7457a3f717',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
